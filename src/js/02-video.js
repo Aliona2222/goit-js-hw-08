050f6cfc01dbf5628ebe40307f63750f6173dd58
Aliona2222 <aliona.prybaten@gmail.com>
2023-07-26 00:17:34 +0300

    Update_3_tasks

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,3 +1,18 @@
-
 import Player from '@vimeo/player';
-console.log(Player);
+import throttle from 'lodash.throttle';
+
+const playerElement = document.getElementById('vimeo-player');
+const player = new Player(playerElement);
+
+const saveCurrentTime = throttle((event) => {
+  player.getCurrentTime().then((currentTime) => {
+    localStorage.setItem('videoplayer-current-time', currentTime);
+  });
+}, 1000);
+
+player.on('timeupdate', saveCurrentTime);
+
+const storedTime = localStorage.getItem('videoplayer-current-time');
+if (storedTime) {
+  player.setCurrentTime(parseFloat(storedTime));
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from 'lodash.throttle';
+
+const feedbackForm = document.querySelector('.feedback-form');
+const emailInput = feedbackForm.querySelector('input[name="email"]');
+const messageInput = feedbackForm.querySelector('textarea[name="message"]');
+
+const saveFormState = throttle(() => {
+  const formState = {
+    email: emailInput.value,
+    message: messageInput.value
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formState));
+}, 500);
+
+const loadFormState = () => {
+  const storedState = localStorage.getItem('feedback-form-state');
+  if (storedState) {
+    const formState = JSON.parse(storedState);
+    emailInput.value = formState.email;
+    messageInput.value = formState.message;
+  }
+};
+
+const submitForm = (event) => {
+  event.preventDefault();
+  
+  const formState = {
+    email: emailInput.value,
+    message: messageInput.value
+  };
+  
+  console.log(formState);
+  
+  localStorage.removeItem('feedback-form-state');
+  emailInput.value = '';
+  messageInput.value = '';
+};
+
+feedbackForm.addEventListener('input', saveFormState);
+window.addEventListener('load', loadFormState);
+feedbackForm.addEventListener('submit', submitForm);
 
